38d4f2932232e2d41444b71d1b61f936f52180f2
Tristan <atkinsontristan13@gmail.com>
2023-11-05 16:04:21 -0400

    commit

--- resources/views/FAQ/index.blade.php
+++ resources/views/FAQ/index.blade.php
@@ -1,8 +1,24 @@
 <x-app-layout>
     @auth
     <div class="container mx-auto flex flex-col items-center text-white">
-    <div class="flex gap-10">
-        <h1 class="text-2xl font-semibold mt-4">Frequently Asked Questions</h1>
+        <div class="flex gap-10">
+            <h1 class="text-2xl font-semibold mt-4">Frequently Asked Questions</h1>
+        </div>
+        <br>
+        <br>
+        <div class="my-8">
+            @foreach ($FAQs->groupBy('Category') as $category => $faqs)
+            <h2 class="text-xl font-semibold text-white">{{ $category }}</h2>
+            <div class="flex flex-col gap-4 w-[400px]">
+            @foreach ($faqs as $faq)
+            <div class="text-lg text-white">Q: {{ $faq->Questions }}</div>
+            <div class="text-lg text-white">A: {{ $faq->Answers }}</div>
+            <br>
+            @endforeach
+            <br>
+            <br>
+            </div>
+            @endforeach
         <div class="relative">
             <a href="{{ route('dashboard') }}" class="absolute left-0 top-0 text-blue-500 hover:text-blue-600 font-semibold text-lg">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline align-text-top" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -11,21 +27,7 @@
                 Back
             </a>
         </div>
-    </div>
-    <div class="my-8">
-        @foreach ($FAQs->groupBy('Category') as $category => $faqs)
-        <h2 class="text-xl font-semibold text-white">{{ $category }}</h2>
-        <div class="flex flex-col gap-4 w-[400px]">
-        @foreach ($faqs as $faq)
-        <div class="text-lg text-white">Q: {{ $faq->Questions }}</div>
-        <div class="text-lg text-white">A: {{ $faq->Answers }}</div>
-        <br>
-        @endforeach
-        <br>
-        <br>
         </div>
-        @endforeach
-    </div>
     </div>
     @endauth
 </x-app-layout>
