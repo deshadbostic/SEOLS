5b1ff54a367c580744098bb9b7f050fa85f1724d
Tristan <atkinsontristan13@gmail.com>
2023-11-04 20:32:52 -0400

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,6 +29,13 @@ class DatabaseSeeder extends Seeder
             HouseInfoSeeder::class,
         ]);
 
+        $this->call([
+            FAQSeeder::class,
+        ]);
+        $this->call([
+            AdminSeeder::class,
+        ]);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
--- resources/views/FAQ/index.blade.php
+++ resources/views/FAQ/index.blade.php
@@ -2,23 +2,24 @@
     @auth
     <div class="container mx-auto flex flex-col items-center text-white">
     <div class="flex gap-10">
-        <h1 class="text-2xl font-semibold mt-4">FAQ</h1>
+        <h1 class="text-2xl font-semibold mt-4">Frequently Asked Questions</h1>
         <div class="relative">
-        <a href="{{ route('FAQs.index') }}" class="absolute left-0 top-0 text-blue-500 hover:text-blue-600 font-semibold text-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline align-text-top" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-            </svg>
-            Back
-        </a>
+            <a href="{{ route('dashboard') }}" class="absolute left-0 top-0 text-blue-500 hover:text-blue-600 font-semibold text-lg">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline align-text-top" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                </svg>
+                Back
+            </a>
         </div>
     </div>
     <div class="my-8">
         @foreach ($FAQs->groupBy('Category') as $category => $faqs)
         <h2 class="text-xl font-semibold text-white">{{ $category }}</h2>
-        <div class="flex flex-col gap-4 w-[280px]">
+        <div class="flex flex-col gap-4 w-[400px]">
         @foreach ($faqs as $faq)
         <div class="text-lg text-white">Q: {{ $faq->Questions }}</div>
         <div class="text-lg text-white">A: {{ $faq->Answers }}</div>
+        <br>
         @endforeach
         <br>
         <br>
