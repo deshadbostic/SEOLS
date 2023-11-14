7058c220383cffc207341ef132decc9515b821b1
Aquon Bovell <aquon_bovell@outlook.com>
2023-11-14 12:34:43 -0400

    added searching and filtering

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -109,8 +109,8 @@
         </div>
       </div>
       <div class="self-end">
-        <a href="/reset">
-          <x-primary-button class=" dark:active:bg-white dark:focus-visible:bg-white dark:focus-within:bg-white w-fit" type="button">{{ __('Reset') }}</x-primary-button>
+        <a href="/reset" class=" dark:focus-visible:bg-white dark:focus-within:bg-white w-fit bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 shadow-sm shadow-slate-950 px-3 py-2 mr-2">
+          {{ __('Reset') }}
         </a>
         <x-primary-button class=" dark:active:bg-white dark:focus-visible:bg-white dark:focus-within:bg-white  w-fit">{{ __('Apply') }}</x-primary-button>
       </div>
