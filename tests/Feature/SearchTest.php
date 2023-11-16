21c51167c94c6035539d8fa63a69400dd0255455
Aquon Bovell <aquon_bovell@outlook.com>
2023-11-16 07:29:11 -0400

    added search test cases

--- tests/Feature/SearchTest.php
+++ tests/Feature/SearchTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -31,5 +32,22 @@ class SearchTest extends TestCase
 
         // Assertions
         $response->assertStatus(302)->assertSessionHas('search', $searchTerm); // Ensure that the response is a redirect
+    }
+
+    public function test_search_products(): void
+    {
+        $product = Product::factory()->create();
+        $user = User::factory()->create();
+
+        // Simulate authentication
+        $this->actingAs($user);
+
+        // Perform a search with a specific term
+        $response = $this->withSession(['search' => $product->Name])
+            ->get('/product');
+
+        // Assertions
+        $response->assertStatus(200);
+        $response->assertSee($product->Name);
     }
 }
