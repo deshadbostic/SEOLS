b6a9837f594248156ba4a60b879ba17c43718871
Aquon Bovell <aquon_bovell@outlook.com>
2023-11-15 22:36:13 -0400

    added the user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,19 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
+            'password' => '12345678', // password
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'role' => 'Customer',
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'phone' => fake()->phoneNumber(),
+            'address' => fake()->address(),
+            'budget' => fake()->numberBetween(20000, 100000),
+            'visited' => false,
+            // Define other attributes as needed
         ];
     }
 
