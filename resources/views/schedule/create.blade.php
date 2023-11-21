a0ef59884ec80bfa8bd296f03f19ccbed7db5d1e
AJ <71909585+RoseFall@users.noreply.github.com>
2023-11-21 12:23:54 -0400

    Update create.blade.php

--- resources/views/schedule/create.blade.php
+++ resources/views/schedule/create.blade.php
@@ -1,16 +1,42 @@
 <head>
     <meta charset="utf-8">
     <title>Schedule an Appointment</title>
+    <script>
+        function setMinDate() {
+            let currDate = new Date();
+            currDate.setDate((currDate.getDate()) + 1);
+            let dateString = currDate.toLocaleDateString('fr-ca');
+            document.getElementById("date").setAttribute('min', dateString);
+        } 
+
+        window.addEventListener("DOMContentLoaded", (event) => {
+            const picker = document.getElementById("date");
+            picker.addEventListener('input', function(e) {
+                var day = new Date(this.value).getUTCDay();
+                if([6,0].includes(day)) {
+                    e.preventDefault();
+                    this.value = '';
+                    alert('Weekends not allowed');
+                }
+            });
+        });
+    </script>
 </head>
 
 <body>
     <x-app-layout>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <a href="javascript:history.back()" class="  text-blue-500 hover:text-blue-600 font-semibold text-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline align-text-top" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                    </svg>
+                    Back
+                </a>
                 <div class="bg-gray-700 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-white">
                         <p>Please fill out the form below to apply for a home / building visit appoinment.<br>
-                            Home visit appointments are only available Mondays to Fridays, from 9:00A.M. to 3:00P.M.
+                            Home visit appointments are only available Mondays to Fridays, from 9:00A.M. to 3:00P.M. by the hour.
                         </p>
                     </div>
                 </div>
