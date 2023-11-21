8c60967da45375eb5a1e0da3b3a65995a44800a0
AJ <71909585+RoseFall@users.noreply.github.com>
2023-11-21 12:17:57 -0400

    Update 2023_10_08_235206_creates_schedule_table.php
    Added 'user_id' field

--- database/migrations/2023_10_08_235206_creates_schedule_table.php
+++ database/migrations/2023_10_08_235206_creates_schedule_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('schedule', function (Blueprint $table) {
             $table->id('id');
+            $table->integer('user_id');
             $table->string('fName');
             $table->string('lName');
             $table->string('address');
