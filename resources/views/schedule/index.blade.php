0c1883469921bad1148f8d78139ee79fa3fc50c6
AJ <71909585+RoseFall@users.noreply.github.com>
2023-11-21 12:22:10 -0400

    Update index.blade.php

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -7,6 +7,12 @@
     @if(Auth::user()->role == "operationsManager")
         <div class="flex pt-5 justify-center min-h-screen bg-gray-900 grid-cols-14">
             <div class="col-span-12">
+                <a href="javascript:history.back()" class="  text-blue-500 hover:text-blue-600 font-semibold text-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline align-text-top" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                    </svg>
+                    Back
+                </a>
                 <div class="overflow-auto lg:overflow-visible ">
                     <!-- Table for Appointments -->
                     <table class="table text-gray-400 border-separate space-y-6 text-sm">
